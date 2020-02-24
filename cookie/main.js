38e23a96732474663357ba9ea4e518b1edb20e42
Christian9901 <34829888+Christian9901@users.noreply.github.com>
2020-02-24 10:56:07 -0600

    Update main.js

--- cookie/main.js
+++ cookie/main.js
@@ -6197,21 +6197,21 @@ Game.Launch=function()
 			
 			if (list.length==0)
 			{
-				if (Game.cookiesEarned<5) list.push('You feel like making cookies. But nobody wants to eat your cookies.');
-				else if (Game.cookiesEarned<50) list.push('Your first batch goes to the trash. The neighborhood raccoon barely touches it.');
-				else if (Game.cookiesEarned<100) list.push('Your family accepts to try some of your cookies.');
-				else if (Game.cookiesEarned<500) list.push('Your cookies are popular in the neighborhood.');
-				else if (Game.cookiesEarned<1000) list.push('People are starting to talk about your cookies.');
-				else if (Game.cookiesEarned<5000) list.push('Your cookies are talked about for miles around.');
-				else if (Game.cookiesEarned<10000) list.push('Your cookies are renowned in the whole town!');
-				else if (Game.cookiesEarned<50000) list.push('Your cookies bring all the boys to the yard.');
-				else if (Game.cookiesEarned<100000) list.push('Your cookies now have their own website!');
-				else if (Game.cookiesEarned<500000) list.push('Your cookies are worth a lot of money.');
-				else if (Game.cookiesEarned<1000000) list.push('Your cookies sell very well in distant countries.');
-				else if (Game.cookiesEarned<5000000) list.push('People come from very far away to get a taste of your cookies.');
-				else if (Game.cookiesEarned<10000000) list.push('Kings and queens from all over the world are enjoying your cookies.');
-				else if (Game.cookiesEarned<50000000) list.push('There are now museums dedicated to your cookies.');
-				else if (Game.cookiesEarned<100000000) list.push('A national day has been created in honor of your cookies.');
+				if (Game.cookiesEarned<5) list.push('oh NO!!! " Chungus" Needs to make " BABY" (baby chu,ngus) ');
+				else if (Game.cookiesEarned<50) list.push('"chungus" (fat bugs bunny) is starting to produce BABY');
+				else if (Game.cookiesEarned<100) list.push('PEOPLE are staring to adopt some BABY "chungus"');
+				else if (Game.cookiesEarned<500) list.push('there are is lot of people adopting baby "chungus" .');
+				else if (Game.cookiesEarned<1000) list.push('"people" are talkign about the baby chungus .');
+				else if (Game.cookiesEarned<5000) list.push('more "people"s are talking about "CHUNGUS"');
+				else if (Game.cookiesEarned<10000) list.push('The TOWN of WINDOW VILLE are talking about " CHUNGUS  BABY".');
+				else if (Game.cookiesEarned<50000) list.push('there are GAY MALES adopting " CHUNGUS".');
+				else if (Game.cookiesEarned<100000) list.push('www.chungustown.com');
+				else if (Game.cookiesEarned<500000) list.push('"CHUNGUS" are worth lot.');
+				else if (Game.cookiesEarned<1000000) list.push('"PEOPLE" from DISTANT LANDS are talking about " CHUNGUS".');
+				else if (Game.cookiesEarned<5000000) list.push('People are TRAVLINING From PAST to get "CHUNGUS BABY');
+				else if (Game.cookiesEarned<10000000) list.push('KINGDOM RULERS like having CHUNGUS.');
+				else if (Game.cookiesEarned<50000000) list.push('APRIL 12, 2030: There Is MUSEUM ABOUT CHUNGUS .');
+				else if (Game.cookiesEarned<100000000) list.push('MAY 20 BEOMES NATIONAL "CHUNGUS" DAY . ');
 				else if (Game.cookiesEarned<500000000) list.push('Your cookies have been named a part of the world wonders.');
 				else if (Game.cookiesEarned<1000000000) list.push('History books now include a whole chapter about your cookies.');
 				else if (Game.cookiesEarned<5000000000) list.push('Your cookies have been placed under government surveillance.');
